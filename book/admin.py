3998d3a0909ad96ac6a9fbe770a8ba57f0deb982
xiejiahui <2165304855@qq.com>
2024-11-11 15:43:26 +0800

    第一次提交

---
+++ book/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Book,Reviews
+
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','description')
+    search_fields = ['title','description']
+    list_editable = ('description',)
+
+admin.site.register(Book,BookAdmin)
+# admin.site.register(Book)
+admin.site.register(Reviews)
+
